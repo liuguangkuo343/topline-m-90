b1f3239e383665be5006b1d26e54d39f52d48edb
liuguangkuo343 <694403653@qq.com>
2020-01-06 11:46:43 +0800

    项目初始化-引入want-按需加载

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 引入
+import { Button } from 'vant'
+// 注册
+Vue.use(Button)
 Vue.config.productionTip = false
 
 new Vue({
